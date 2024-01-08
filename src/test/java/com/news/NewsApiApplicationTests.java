31930465c0ad2379e7a209a580d221d7e4924dcd
Alexandre Martins Garcia <alegarci@br.ibm.com>
2024-01-08 16:42:51 -0300

    chore: add profiles spring boot

--- src/test/java/com/news/NewsApiApplicationTests.java
+++ src/test/java/com/news/NewsApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.news;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "junit")
 class NewsApiApplicationTests {
 
 	@Test
