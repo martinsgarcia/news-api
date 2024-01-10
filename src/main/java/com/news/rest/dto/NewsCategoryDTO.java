516703c7a8770270f3732a2bc80ac7a245a0f6c8
Alexandre Martins Garcia <alegarci@br.ibm.com>
2024-01-10 16:02:58 -0300

    test: add test methods get/create/update/delete of service NewsCategoryService

--- src/main/java/com/news/rest/dto/NewsCategoryDTO.java
+++ src/main/java/com/news/rest/dto/NewsCategoryDTO.java
@@ -2,10 +2,14 @@ package com.news.rest.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Schema(description = "News Category DTO")
 public class NewsCategoryDTO {
--- src/test/java/com/news/service/NewsCategoryServiceTest.java
+++ src/test/java/com/news/service/NewsCategoryServiceTest.java
@@ -1,6 +1,8 @@
 package com.news.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.news.persistence.entity.NewsCategoryEntity;
 import com.news.persistence.repository.NewsCategoryRepository;
+import com.news.rest.dto.NewsCategoryDTO;
 
 @SpringBootTest
 @ActiveProfiles("junit")
@@ -35,6 +38,62 @@ class NewsCategoryServiceTest {
 		assertEquals(2, result.size());
 		assertEquals("Category1", result.get(0).getTitle());
 		assertEquals("Category2", result.get(1).getTitle());
+	}
+
+	@Test
+	void testGet() {
+
+		newsCategoryRepository.save(new NewsCategoryEntity(1L, "Category1", "Description1"));
+
+		NewsCategoryEntity result = newsCategoryService.get(1L);
+
+		// Assert
+		assertEquals("Category1", result.getTitle());
+	}
+
+	@Test
+	void testCreate() {
+
+		NewsCategoryDTO newsCategoryDTO = new NewsCategoryDTO();
+		newsCategoryDTO.setId(1L);
+		newsCategoryDTO.setTitle("Category1");
+		newsCategoryDTO.setDescription("Description1");
+
+		newsCategoryService.create(newsCategoryDTO);
+
+		NewsCategoryEntity result = newsCategoryRepository.findById(1L).orElse(null);
+
+		assertNotNull(result);
+		assertEquals("Category1", result.getTitle());
+		assertEquals("Description1", result.getDescription());
+	}
+
+	@Test
+	void testUpdate() {
+
+		newsCategoryRepository.save(new NewsCategoryEntity(1L, "Category1", "Description1"));
+
+		NewsCategoryDTO newsCategoryDTO = new NewsCategoryDTO();
+		newsCategoryDTO.setTitle("UpdatedCategory");
+		newsCategoryDTO.setDescription("UpdatedDescription");
+
+		NewsCategoryEntity result = newsCategoryService.update(1L, newsCategoryDTO);
+
+		// Assert
+		assertEquals("UpdatedCategory", result.getTitle());
+		assertEquals("UpdatedDescription", result.getDescription());
+	}
+
+	@Test
+	void testDelete() {
+
+		newsCategoryRepository.save(new NewsCategoryEntity(1L, "Category1", "Description1"));
+
+		newsCategoryService.delete(1L);
+
+		NewsCategoryEntity result = newsCategoryRepository.findById(1L).orElse(null);
+
+		assertNull(result);
 	}
 
 }
