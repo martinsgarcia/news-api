69f8de42eb8da32b1cdfcc152d5770cee9dc7630
Alexandre Martins Garcia <alegarci@br.ibm.com>
2024-01-10 16:45:14 -0300

    test: add test inject main test

--- src/test/java/com/news/NewsApiApplicationTests.java
+++ src/test/java/com/news/NewsApiApplicationTests.java
@@ -1,15 +1,24 @@
 package com.news;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.news.service.NewsService;
+
 @SpringBootTest
-@ActiveProfiles(profiles = "junit")
+@ActiveProfiles("junit")
 class NewsApiApplicationTests {
+
+	@Autowired
+	private NewsService service;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(service);
 	}
 
 }
