a6623477e97a39f89663d4589b7e246f233b65c8
Alexandre Martins Garcia <alegarci@br.ibm.com>
2024-01-11 10:38:11 -0300

    test: Junit main class

--- src/test/java/com/news/NewsApiApplicationTests.java
+++ src/test/java/com/news/NewsApiApplicationTests.java
@@ -1,24 +1,19 @@
 package com.news;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import com.news.service.NewsService;
 
 @SpringBootTest
 @ActiveProfiles("junit")
 class NewsApiApplicationTests {
 
-	@Autowired
-	private NewsService service;
-
 	@Test
 	void contextLoads() {
-		assertNotNull(service);
+		String[] args = {};
+		assertDoesNotThrow(() -> NewsApiApplication.main(args));
 	}
 
 }
