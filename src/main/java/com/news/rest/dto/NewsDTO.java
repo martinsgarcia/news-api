315173c34b49d13fd440a70904b1543f960e2663
Alexandre Martins Garcia <alegarci@br.ibm.com>
2024-01-11 10:27:06 -0300

    refactor: removed unnecessary method

--- src/main/java/com/news/rest/dto/NewsDTO.java
+++ src/main/java/com/news/rest/dto/NewsDTO.java
@@ -3,11 +3,9 @@ package com.news.rest.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
-@Setter
 @Schema(description = "News DTO")
 public class NewsDTO {
 
